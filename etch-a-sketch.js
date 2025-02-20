b72814cdfa1cad16871d0b2f0ca85ec0ce029ece
144380980+cyrusrogers@users.noreply.github.com <nderitcyrus@gmail.com>
2025-02-20 12:26:15 +0300

    creating html file

---
+++ etch-a-sketch.js
@@ -0,0 +1,18 @@
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container)
+
+
+for (let i = 0; i < 16; i++) {
+  const innerDiv = document.createElement("div");
+  innerDiv.classList.add("inner-div");
+
+  for (let j = 0; j < 16; j++) {
+    const innerDiv2 = document.createElement("div");
+    innerDiv2.classList.add("inner-div2");
+    innerDiv.append(innerDiv2)
+    
+  }
+  container.append(innerDiv);
+}
+
